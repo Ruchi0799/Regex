3eeb7695525c90fccb5dfc93e3f6918aa2ad2f6d
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-28 01:50:50 +0530

    [ruchi]: implemented validate emailid for User Regstration

--- src/com/bridgelabz/userregistration/Regex.java
+++ src/com/bridgelabz/userregistration/Regex.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Regex {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("What do you want to Validate 1.Firstname \n 2.Lastname");
+        System.out.println("What do you want to Validate 1.Firstname \n 2.Lastname  \n 3.EmailId");
         int choice= sc.nextInt();
         switch (choice){
             case 1:
@@ -27,13 +27,25 @@ public class Regex {
                 String regexln="[A-Z][a-z]{2,}";
                 if (LastName.matches(regexln))
                 {
-                    System.out.println("First Name is Valid");
+                    System.out.println("Last Name is Valid");
                 }
                 else {
-                    System.out.println("Invalid FirstName");
+                    System.out.println("Invalid LastName");
                 }
                 break;
 
+            case 3:
+                System.out.println("Enter EmailId");
+                String emailId=sc.next();
+                String regexEI= "[a-zA-Z0-9_-]+[.{1}]*[a-zA-Z0-9]*[@][a-zA-Z0-9]{2,}[.][a-zA-Z]{2,}[.]*[a-zA-Z{2}]*";
+                if (emailId.matches(regexEI))
+                {
+                    System.out.println("Email ID is Valid");
+                }
+                else {
+                    System.out.println("Invalid Email ID");
+                }
+                break;
         }
     }
 }
