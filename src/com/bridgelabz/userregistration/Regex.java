66e4db7fde11435bdb3848c156b9f42f284ac91d
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-08-28 01:38:30 +0530

    [ruchi]: implemented validate lastname for User Regstration

--- src/com/bridgelabz/userregistration/Regex.java
+++ src/com/bridgelabz/userregistration/Regex.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Regex {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("What do you want to Validate 1.Firstname");
+        System.out.println("What do you want to Validate 1.Firstname \n 2.Lastname");
         int choice= sc.nextInt();
         switch (choice){
             case 1:
@@ -20,6 +20,20 @@ public class Regex {
                     System.out.println("Invalid FirstName");
                 }
                 break;
+
+            case 2:
+                System.out.println("Enter LastName");
+                String LastName=sc.next();
+                String regexln="[A-Z][a-z]{2,}";
+                if (LastName.matches(regexln))
+                {
+                    System.out.println("First Name is Valid");
+                }
+                else {
+                    System.out.println("Invalid FirstName");
+                }
+                break;
+
         }
     }
 }
